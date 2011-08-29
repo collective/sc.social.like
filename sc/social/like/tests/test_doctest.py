434ebecee352eac26aabb9aacd6fc61b64f4065e
Héctor Velarde <hector.velarde@gmail.com>
2011-08-29 11:59:14 -0300

    fixed package name

--- sc/social/like/tests/test_doctest.py
+++ sc/social/like/tests/test_doctest.py
@@ -5,7 +5,7 @@ import doctest
 
 from plone.testing import layered
 
-from collective.nitf.testing import FUNCTIONAL_TESTING
+from sc.social.like.testing import FUNCTIONAL_TESTING
 
 optionflags = doctest.REPORT_ONLY_FIRST_FAILURE
 
@@ -16,6 +16,6 @@ def test_suite():
         layered(doctest.DocFileSuite('functional.txt',
                                      optionflags=optionflags),
                 layer=FUNCTIONAL_TESTING),
-        doctest.DocTestSuite(module='collective.nitf'),
+        doctest.DocTestSuite(module='sc.social.like'),
     ])
     return suite
