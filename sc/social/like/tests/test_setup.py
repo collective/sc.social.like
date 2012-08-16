01aae501d2da26769d3bf57bd0a5661787908643
hvelarde <hector.velarde@gmail.com>
2012-08-16 17:06:09 -0300

    added basic installation tests

---
+++ sc/social/like/tests/test_setup.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+
+from plone.browserlayer.utils import registered_layers
+
+from sc.social.like.config import PROJECTNAME
+from sc.social.like.testing import INTEGRATION_TESTING
+
+
+JAVASCRIPTS = [
+    ]
+
+CSS = [
+    '++resource++sl_stylesheets/social_like.css',
+    ]
+
+class InstallTestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_installed(self):
+        qi = getattr(self.portal, 'portal_quickinstaller')
+        self.assertTrue(qi.isProductInstalled(PROJECTNAME))
+
+    def test_addon_layer(self):
+        layers = [l.getName() for l in registered_layers()]
+        self.assertTrue('ISocialLikeLayer' in layers,
+                        'add-on layer was not installed')
+
+    def test_jsregistry(self):
+        resource_ids = self.portal.portal_javascripts.getResourceIds()
+        for id in JAVASCRIPTS:
+            self.assertTrue(id in resource_ids, '%s not installed' % id)
+
+    def test_cssregistry(self):
+        resource_ids = self.portal.portal_css.getResourceIds()
+        for id in CSS:
+            self.assertTrue(id in resource_ids, '%s not installed' % id)
+
+
+class UninstallTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.qi = getattr(self.portal, 'portal_quickinstaller')
+        self.qi.uninstallProducts(products=[PROJECTNAME])
+
+    def test_uninstalled(self):
+        self.assertFalse(self.qi.isProductInstalled(PROJECTNAME))
+
+    def test_addon_layer_removed(self):
+        layers = [l.getName() for l in registered_layers()]
+        self.assertFalse('ISocialLikeLayer' in layers,
+                         'add-on layer was not removed')
+
+    def test_jsregistry_removed(self):
+        resource_ids = self.portal.portal_javascripts.getResourceIds()
+        for id in JAVASCRIPTS:
+            self.assertTrue(id not in resource_ids, '%s not removed' % id)
+
+    def test_cssregistry_removed(self):
+        resource_ids = self.portal.portal_css.getResourceIds()
+        for id in CSS:
+            self.assertTrue(id not in resource_ids, '%s not removed' % id)
