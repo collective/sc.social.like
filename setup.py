b5018683e555e8a8e03fa9c20bda363f3f68751a
Érico Andrei <erico@simplesconsultoria.com.br>
2013-03-26 10:25:44 -0300

    PEP8 and PyFlakes

--- setup.py
+++ setup.py
@@ -3,35 +3,37 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.1.dev0'
-long_description = open("README.txt").read() + "\n" + \
-                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
-                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
-                   open(os.path.join("docs", "HISTORY.txt")).read()
+long_description = (open("README.txt").read() + "\n" +
+                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
+                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.txt")).read())
 
 setup(name='sc.social.like',
       version=version,
-      description="sc.social.like is a Plone package providing simple Google+, Twitter and Facebook integration for Plone Content Types",
+      description="""Social Like is a Plone package providing simple Google+,
+                     Twitter and Facebook integration for Plone
+                     Content Types""",
       long_description=long_description,
       classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Framework :: Plone :: 3.3",
-        "Framework :: Plone :: 4.0",
-        "Framework :: Plone :: 4.1",
-        "Framework :: Plone :: 4.2",
-        "Framework :: Plone :: 4.3",
-        "Intended Audience :: End Users/Desktop",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Operating System :: OS Independent",
-        "Programming Language :: JavaScript",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Office/Business :: News/Diary",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Intended Audience :: End Users/Desktop",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: JavaScript",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Office/Business :: News/Diary",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='python plone zope webdev social googleplus facebook twitter',
       author='Simples Consultoria',
       author_email='products@simplesconsultoria.com.br',
@@ -42,12 +44,12 @@ setup(name='sc.social.like',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        'setuptools',
-        'Plone',
-        ],
+          'setuptools',
+          'Plone',
+      ],
       extras_require={
-        'test': ['plone.app.testing'],
-        },
+          'test': ['plone.app.testing']
+      },
       entry_points="""
       [z3c.autoinclude.plugin]
       target = plone
