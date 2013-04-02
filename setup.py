c3fd892a839612e46168739370ea833b14fc45ef
hvelarde <hector.velarde@gmail.com>
2013-04-02 12:02:49 -0300

    use correct .rst extension on documentation files

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.0.2.dev0'
-long_description = (open("README.txt").read() + "\n" +
-                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
-                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read())
+long_description = (open("README.rst").read() + "\n" +
+                    open(os.path.join("docs", "INSTALL.rst")).read() + "\n" +
+                    open(os.path.join("docs", "CREDITS.rst")).read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.rst")).read())
 
 setup(name='sc.social.like',
       version=version,
