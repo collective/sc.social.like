91aa270b6f3b546e1a1a0bea1ed8b4918348f84b
Érico Andrei <erico@simplesconsultoria.com.br>
2013-06-14 18:08:50 -0300

    Control panel only deals with the add on config. All other confis were moved to plugins

--- sc/social/like/controlpanel/likes.py
+++ sc/social/like/controlpanel/likes.py
@@ -1,45 +1,37 @@
 # -*- coding:utf-8 -*-
-
-from zope.schema import Bool
-from zope.schema import TextLine
-from zope.schema import Tuple
-from zope.schema import Choice
-from zope.component import adapts
-from zope.interface import Interface
-from zope.interface import implements
-
+from Acquisition import aq_inner
+from plone.app.controlpanel.form import ControlPanelForm
+from Products.CMFDefault.formlib.schema import ProxyFieldProperty as PFP
+from Products.CMFDefault.formlib.schema import SchemaAdapterBase
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.CMFPlone.utils import getToolByName
-from Products.CMFDefault.formlib.schema import ProxyFieldProperty
-from Products.CMFDefault.formlib.schema import SchemaAdapterBase
-
-from plone.app.controlpanel.form import ControlPanelForm
-
-from plone.fieldsets.fieldsets import FormFieldsets
-
-from zope.app.form.browser.itemswidgets import MultiSelectWidget as BaseMultiSelectWidget
-
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from sc.social.like import LikeMessageFactory as _
+from sc.social.like.plugins import IPlugin
+from zope import schema
+from zope.app.form.browser import itemswidgets
+from zope.component import adapts
+from zope.component import getUtilitiesFor
+from zope.formlib.form import FormFields
+from zope.interface import implements
+from zope.interface import Interface
 
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-
-verbs = SimpleVocabulary([
-    SimpleTerm(value=u'like', title=_(u'Like')),
-    SimpleTerm(value=u'recommend', title=_(u'Recommend')),
-])
+CONTENT_TYPES = 'plone.app.vocabularies.ReallyUserFriendlyTypes'
 
 
-class MultiSelectWidget(BaseMultiSelectWidget):
+class MultiSelectWidget(itemswidgets.MultiSelectWidget):
     """ """
     def __init__(self, field, request):
         """Initialize the widget."""
-        super(MultiSelectWidget, self).__init__(field, field.value_type.vocabulary, request)
+        super(MultiSelectWidget, self).__init__(field,
+                                                field.value_type.vocabulary,
+                                                request)
 
 
 class IProvidersSchema(Interface):
     """ General Configurations """
 
-    enabled_portal_types = Tuple(
+    enabled_portal_types = schema.Tuple(
         title=_(u'Content types'),
         description=_(
             u'help_portal_types',
@@ -47,10 +39,20 @@ class IProvidersSchema(Interface):
                     u"viewlet will be applied.",
         ),
         required=True,
-        value_type=Choice(vocabulary="plone.app.vocabularies.ReallyUserFriendlyTypes")
+        value_type=schema.Choice(vocabulary=CONTENT_TYPES)
     )
 
-    typebutton = Choice(
+    plugins_enabled = schema.Tuple(
+        title=_(u'Plugins'),
+        description=_(
+            u'help_enabled_plugins',
+            default=u"Please select which plugins will be used",
+        ),
+        required=False,
+        value_type=schema.Choice(vocabulary='sc.social.likes.plugins')
+    )
+
+    typebutton = schema.Choice(
         title=_(u'Button style'),
         description=_(
             u'help_selected_buttons',
@@ -59,69 +61,6 @@ class IProvidersSchema(Interface):
         required=True,
         default=_(u'horizontal'),
         values=(_(u'horizontal'), _(u'vertical')),
-    )
-
-
-class ITwitterSchema(Interface):
-    """ Twitter configurations """
-
-    twitter_enabled = Bool(
-        title=_(u"Enable Twitter button"),
-        default=True,
-        required=False,
-    )
-
-    twittvia = TextLine(
-        title=_(u'Twitter nick'),
-        description=_(
-            u'help_your_twitter_nick',
-            default=u"Enter your twitter nick. eg. simplesconsultoria",
-        ),
-        required=False,
-    )
-
-
-class IFbSchema(Interface):
-    """ Facebook configurations """
-
-    fb_enabled = Bool(
-        title=_(u"Enable Facebook button"),
-        default=True,
-        required=False,
-    )
-
-    fbaction = Choice(
-        title=_(u'Verb to display'),
-        description=_(
-            u'help_verb_display',
-            default=u"The verb to display in the facebook button. "
-                    u"Currently only 'like' and 'recommend' are "
-                    u"supported.",
-        ),
-        required=True,
-        default=u'like',
-        vocabulary=verbs,
-    )
-
-    fbadmins = TextLine(
-        title=_(u'Admins'),
-        description=_(
-            u'help_admins',
-            default=u"A comma-separated list of either the "
-                    u"Facebook IDs of page administrators or a "
-                    u"Facebook Platform application ID.",
-        ),
-        required=False,
-    )
-
-
-class IGpSchema(Interface):
-    """ Google+ configurations """
-
-    gp_enabled = Bool(
-        title=_(u"Enable Google+ action"),
-        default=True,
-        required=False,
     )
 
 
@@ -139,59 +78,34 @@ class LikeControlPanelAdapter(BaseControlPanelAdapter):
     adapts(IPloneSiteRoot)
     implements(IProvidersSchema)
 
-    enabled_portal_types = ProxyFieldProperty(IProvidersSchema['enabled_portal_types'])
-    typebutton = ProxyFieldProperty(IProvidersSchema['typebutton'])
-
-
-class TwitterControlPanelAdapter(BaseControlPanelAdapter):
-    """ Twitter control panel adapter """
-    adapts(IPloneSiteRoot)
-    implements(ITwitterSchema)
-
-    twitter_enabled = ProxyFieldProperty(ITwitterSchema['twitter_enabled'])
-    twittvia = ProxyFieldProperty(ITwitterSchema['twittvia'])
-
-
-class FbControlPanelAdapter(BaseControlPanelAdapter):
-    """ Facebook control panel adapter """
-    adapts(IPloneSiteRoot)
-    implements(IFbSchema)
-
-    fb_enabled = ProxyFieldProperty(IFbSchema['fb_enabled'])
-    fbaction = ProxyFieldProperty(IFbSchema['fbaction'])
-    fbadmins = ProxyFieldProperty(IFbSchema['fbadmins'])
-
-
-class GpControlPanelAdapter(BaseControlPanelAdapter):
-    """ Google+ control panel adapter """
-    adapts(IPloneSiteRoot)
-    implements(IGpSchema)
-
-    gp_enabled = ProxyFieldProperty(IGpSchema['gp_enabled'])
-
-baseset = FormFieldsets(IProvidersSchema)
-baseset.id = 'baseset'
-baseset.label = _(u'Base Plugin Configuration')
-
-twitterset = FormFieldsets(ITwitterSchema)
-twitterset.id = 'twitterset'
-twitterset.label = _(u'Twitter settings')
-
-fbset = FormFieldsets(IFbSchema)
-fbset.id = 'fbset'
-fbset.label = _(u'Facebook settings')
-
-gpset = FormFieldsets(IGpSchema)
-gpset.id = 'gpset'
-gpset.label = _(u'Google+ settings')
+    enabled_portal_types = PFP(IProvidersSchema['enabled_portal_types'])
+    typebutton = PFP(IProvidersSchema['typebutton'])
+    plugins_enabled = PFP(IProvidersSchema['plugins_enabled'])
 
 
 class ProvidersControlPanel(ControlPanelForm):
     """ """
-    form_fields = FormFieldsets(baseset, twitterset, fbset, gpset)
+    template = ViewPageTemplateFile('likes.pt')
+    form_fields = FormFields(IProvidersSchema)
 
     form_fields['enabled_portal_types'].custom_widget = MultiSelectWidget
+    form_fields['plugins_enabled'].custom_widget = MultiSelectWidget
 
     label = _('Social: Like Actions settings')
     description = _('Configure settings for social like actions.')
     form_name = _('Social: Like Actions')
+
+    def plugins_configs(self):
+        ''' Return Plugins and their configuration pages '''
+        context = aq_inner(self.context)
+        portal_url = getToolByName(context, 'portal_url')()
+        registered = dict(getUtilitiesFor(IPlugin))
+        plugins = []
+        for name in registered:
+            plugin = registered[name]
+            config_view = plugin.config_view()
+            if config_view:
+                url = '%s/%s' % (portal_url, config_view)
+                plugins.append({'name': name,
+                                'url': url})
+        return plugins
