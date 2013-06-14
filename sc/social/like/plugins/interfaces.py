bff6956ae5c8e0b1f0b6d6b218b25e1739731fb2
Érico Andrei <erico@simplesconsultoria.com.br>
2013-06-14 18:34:36 -0300

    Better document Plugin interfaces

--- sc/social/like/plugins/interfaces.py
+++ sc/social/like/plugins/interfaces.py
@@ -3,10 +3,27 @@ from zope.interface import Interface
 
 
 class IPlugin(Interface):
-    pass
+    ''' A Social Like Plugin '''
+
+    id = ''
+    name = ''
+
+    def config_view():
+        pass
+
+    def view(self):
+        pass
+
+    def metadata(self):
+        pass
+
+    def plugin(self):
+        pass
 
 
 class IPluginView(Interface):
+    ''' A Social Like Plugin Browser View '''
+
     def metadata():
         pass
 
