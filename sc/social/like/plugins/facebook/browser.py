9c41da0870cb28662ec774ccc19e21e381fd24de
Érico Andrei <erico@simplesconsultoria.com.br>
2013-06-15 00:16:53 -0300

    Deal with Dexterity content types (as NITF)

--- sc/social/like/plugins/facebook/browser.py
+++ sc/social/like/plugins/facebook/browser.py
@@ -55,6 +55,9 @@ class PluginView(BrowserView):
             field = [field for field in schema.keys() if field in fields]
             if field:
                 field = field[0]
+        else:
+            # Let's assume image as a valid fieldname
+            field = 'image'
         try:
             view = context.unrestrictedTraverse('@@images')
             img = view.scale(fieldname=field, scale=scale)
