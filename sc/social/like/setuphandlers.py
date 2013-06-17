988736338ac5c2607e24e8e0ae7cc57e09c5b8d8
Érico Andrei <erico@simplesconsultoria.com.br>
2013-06-17 19:14:48 -0300

    Hide profiles from installation

--- sc/social/like/setuphandlers.py
+++ sc/social/like/setuphandlers.py
@@ -1,5 +1,27 @@
 # -*- coding:utf-8 -*-
 from Products.CMFCore.utils import getToolByName
+from Products.CMFQuickInstallerTool import interfaces as qi_interfaces
+from Products.CMFPlone import interfaces as plone_interfaces
+from zope.interface import implements
+
+
+class HiddenProducts(object):
+    implements(qi_interfaces.INonInstallable)
+
+    def getNonInstallableProducts(self):
+        return ['sc.social.like.upgrades.v2000',
+                'sc.social.like.upgrades.v3000']
+
+
+class HiddenProfiles(object):
+    implements(plone_interfaces.INonInstallable)
+
+    def getNonInstallableProfiles(self):
+        return [
+            u'sc.social.like:uninstall',
+            u'sc.social.like.upgrades.v2000:default',
+            u'sc.social.like.upgrades.v3000:default',
+        ]
 
 
 def install(context):
