54c04359508c349c178a7a3f31c130fb1af7fb59
Érico Andrei <erico@simplesconsultoria.com.br>
2013-06-19 15:27:15 -0300

    Cache image accesses

--- sc/social/like/utils.py
+++ sc/social/like/utils.py
@@ -1,20 +1,54 @@
 # -*- coding:utf-8 -*-
 from Products.Archetypes.interfaces import IBaseContent
+from zope.annotation.interfaces import IAnnotations
+from zope.globalrequest import getRequest
 
 
-def get_content_image(context, scale='large'):
-    fields = ['image', 'leadImage', 'portrait']
-    if IBaseContent.providedBy(context):
-        schema = context.Schema()
-        field = [field for field in schema.keys() if field in fields]
-        if field:
-            field = field[0]
-    else:
-        # Let's assume image as a valid fieldname
-        field = 'image'
-    try:
-        view = context.unrestrictedTraverse('@@images')
-        img = view.scale(fieldname=field, scale=scale)
-    except AttributeError:
-        img = None
+def get_images_view(context):
+    request = getRequest()
+    key = "cache-view-%s" % (context)
+    cache = IAnnotations(request)
+    value = cache.get(key, None)
+    if not value:
+        view = context.unrestrictedTraverse('@@images', None)
+        if view:
+            fields = ['image', 'leadImage', 'portrait']
+            if IBaseContent.providedBy(context):
+                schema = context.Schema()
+                field = [field for field in schema.keys() if field in fields]
+                if field:
+                    field = field[0]
+            else:
+                # Let's assume image as a valid fieldname
+                field = 'image'
+        value = (view, field)
+        cache[key] = value
+    return value
+
+
+def get_content_image(context,
+                      scale='large',
+                      width=None,
+                      height=None):
+    request = getRequest()
+    key = "cache-%s-%s-%s-%s" % (context, scale, width, height)
+    cache = IAnnotations(request)
+    img = cache.get(key, None)
+    if not img:
+        view, field = get_images_view(context)
+        if view:
+            kwargs = {}
+            if not (width or height):
+                kwargs['scale'] = scale
+            else:
+                if width:
+                    kwargs['width'] = width
+                if height:
+                    kwargs['height'] = height
+                kwargs['direction'] = 'down'
+            try:
+                img = view.scale(fieldname=field, **kwargs)
+            except AttributeError:
+                img = None
+        cache[key] = img
     return img
