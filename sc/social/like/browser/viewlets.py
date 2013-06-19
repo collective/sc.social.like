3cd7021baff8d85c1c8f24d84dd25fc0647a0773
Érico Andrei <erico@simplesconsultoria.com.br>
2013-06-19 15:26:52 -0300

    Refactor viewlets to use the newly created helper view

--- sc/social/like/browser/viewlets.py
+++ sc/social/like/browser/viewlets.py
@@ -1,10 +1,7 @@
 # -*- coding:utf-8 -*-
 from plone.app.layout.viewlets import ViewletBase
-from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from sc.social.like.plugins import IPlugin
 from zope.component import getMultiAdapter
-from zope.component import getUtilitiesFor
 
 
 class BaseLikeViewlet(ViewletBase):
@@ -16,46 +13,18 @@ class BaseLikeViewlet(ViewletBase):
 
     def __init__(self, context, request, view, manager):
         super(BaseLikeViewlet, self).__init__(context, request, view, manager)
-        pp = getToolByName(context, 'portal_properties')
-
         self.context = context
         self.request = request
-        self.portal_state = getMultiAdapter((self.context, self.request),
-                                            name=u'plone_portal_state')
-
-        self.context_state = getMultiAdapter((self.context, self.request),
-                                             name=u'plone_context_state')
-
-        self.site_url = self.portal_state.portal_url()
-        self.sheet = getattr(pp, 'sc_social_likes_properties', None)
-        self.enabled_portal_types = self.sheet.getProperty(
-            'enabled_portal_types',
-            []
-        )
-        self.plugins_enabled = self.sheet.getProperty('plugins_enabled',
-                                                      [])
-
-    def available_plugins(self):
-        registered = dict(getUtilitiesFor(IPlugin))
-        return registered
-
-    def _plugins(self):
-        available = self.available_plugins()
-        enabled = self.plugins_enabled
-        plugins = []
-        for plugin_id in enabled:
-            plugin = available.get(plugin_id, None)
-            if plugin:
-                plugins.append(plugin)
-        return plugins
+        self.helper = getMultiAdapter((self.context, self.request),
+                                      name=u'sl_helper')
 
     def plugins(self):
         context = self.context
         render_method = self.render_method
         rendered = []
-        plugins = self._plugins()
+        plugins = self.helper.plugins()
         for plugin in plugins:
-            if plugin and render_method:
+            if plugin and getattr(plugin, render_method)():
                 view = context.restrictedTraverse(plugin.view())
                 html = getattr(view, render_method)()
                 rendered.append({'id': plugin.id,
@@ -65,9 +34,7 @@ class BaseLikeViewlet(ViewletBase):
     def enabled(self):
         """Validates if the viewlet should be enabled for this context
         """
-        context = self.context
-        enabled_portal_types = self.enabled_portal_types
-        return context.portal_type in enabled_portal_types
+        return self.helper.enabled()
 
     # HACK: fixes https://bitbucket.org/takaki/sc.social.like/issue/1
     def update(self):
@@ -86,16 +53,14 @@ class SocialMetadataViewlet(BaseLikeViewlet):
     def enabled(self):
         """Validates if the viewlet should be enabled for this context
         """
-        context = self.context
-        template = self.context_state.view_template_id()
+        template = self.helper.view_template_id()
         # If using folder_full_view or all_content, we add metadata
         # in order to proper display share buttons for
         # contained content types
         if template in ('all_content', 'folder_full_view',):
             return True
         else:
-            enabled_portal_types = self.enabled_portal_types
-            return context.portal_type in enabled_portal_types
+            return super(SocialMetadataViewlet, self).enabled()
 
 
 class SocialLikesViewlet(BaseLikeViewlet):
