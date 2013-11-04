354e08a28a1147faab7f4c37608f97a002e9ed1a
Érico Andrei <erico@simplesconsultoria.com.br>
2013-11-04 10:35:26 -0200

    Preparing release 2.0a2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 import os
 
-version = '2.0a2.dev0'
+version = '2.0a2'
 long_description = (open("README.rst").read() + "\n" +
                     open(os.path.join("docs", "INSTALL.rst")).read() + "\n" +
                     open(os.path.join("docs", "CREDITS.rst")).read() + "\n" +
