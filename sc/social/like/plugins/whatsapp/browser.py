0d52748f1af110584d0ca2fdffb279221a7c4a9a
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2015-04-28 16:29:54 -0300

    Adding whatsapp plugin

---
+++ sc/social/like/plugins/whatsapp/browser.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import safe_unicode
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.PythonScripts.standard import url_quote
+from sc.social.like.utils import get_language
+from zope.component import getMultiAdapter
+
+
+class PluginView(BrowserView):
+
+    typebutton = ''
+    gp_enabled = True
+    language = 'en'
+
+    metadata = ViewPageTemplateFile("templates/metadata.pt")
+    plugin = ViewPageTemplateFile("templates/plugin.pt")
+
+    def __init__(self, context, request):
+        super(PluginView, self).__init__(context, request)
+        pp = getToolByName(context, 'portal_properties')
+
+        self.context = context
+        self.request = request
+        self.portal_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_portal_state')
+        self.portal = self.portal_state.portal()
+        self.site_url = self.portal_state.portal_url()
+        self.portal_title = self.portal_state.portal_title()
+        self.url = context.absolute_url()
+        self.language = get_language(context)
+        self.sheet = getattr(pp, 'sc_social_likes_properties', None)
+        self.whatsappurl = (
+            u'whatsapp://send?text=' +
+            url_quote(
+                u'{0} - {1}'.format(
+                    safe_unicode(self.context.title),
+                    self.context.absolute_url()
+                )
+            )
+        )
---
+++ sc/social/like/tests/test_plugin_whatsapp.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from sc.social.like.controlpanel.likes import LikeControlPanelAdapter
+from sc.social.like.interfaces import ISocialLikeLayer
+from sc.social.like.plugins.interfaces import IPlugin
+from sc.social.like.plugins.whatsapp import browser
+from sc.social.like.testing import INTEGRATION_TESTING
+from zope.component import getUtilitiesFor
+from zope.interface import alsoProvides
+
+import unittest2 as unittest
+
+name = 'WhatsApp'
+
+
+class PluginTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        alsoProvides(self.portal.REQUEST, ISocialLikeLayer)
+        self.plugins = dict(getUtilitiesFor(IPlugin))
+
+    def test_plugin_available(self):
+        self.assertIn(name, self.plugins)
+
+    def test_plugin_config(self):
+        plugin = self.plugins[name]
+        self.assertEqual(plugin.name, name)
+        self.assertEqual(plugin.id, 'whatsapp')
+
+    def test_plugin_config_view(self):
+        plugin = self.plugins[name]
+        self.assertIsNone(plugin.config_view())
+
+    def test_plugin_view(self):
+        plugin = self.plugins[name]
+        self.assertEqual(plugin.view(), '@@whatsapp-plugin')
+
+    def test_plugin_metadata(self):
+        plugin = self.plugins[name]
+        self.assertEqual(plugin.metadata(), 'metadata')
+
+    def test_plugin_plugin(self):
+        plugin = self.plugins[name]
+        self.assertEqual(plugin.plugin(), 'plugin')
+
+
+class PluginViewsTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.adapter = LikeControlPanelAdapter(self.portal)
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.setup_content(self.portal)
+        alsoProvides(self.portal.REQUEST, ISocialLikeLayer)
+        self.plugins = dict(getUtilitiesFor(IPlugin))
+        self.plugin = self.plugins[name]
+
+    def setup_content(self, portal):
+        portal.invokeFactory('Document', 'my-document')
+        self.document = portal['my-document']
+
+    def test_plugin_view(self):
+        plugin = self.plugin
+        portal = self.portal
+        plugin_view = plugin.view()
+        view = portal.restrictedTraverse(plugin_view)
+        self.assertTrue(isinstance(view, browser.PluginView))
+
+    def test_plugin_view_html(self):
+        plugin = self.plugin
+        document = self.document
+        plugin_view = plugin.view()
+        view = document.restrictedTraverse(plugin_view)
+        html = view.plugin()
+        self.assertIn('whatsapp', html)
+
+    def test_plugin_urlnoscript_encoding(self):
+        plugin = self.plugin
+        document = self.document
+        document.setTitle(u'Notícia')
+
+        plugin_view = plugin.view()
+        view = document.restrictedTraverse(plugin_view)
+        html = view.plugin()
+        self.assertIn(
+            'Not%C3%ADcia%20-%20http%3A//nohost/plone/my-document">Share in WhatsApp',
+            html
+        )
