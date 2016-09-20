af6d015b5994bbe9374f21c0a2333108317f6f88
hvelarde <hector.velarde@gmail.com>
2016-09-20 20:05:29 -0300

    Fix magic comment

--- sc/social/like/setuphandlers.py
+++ sc/social/like/setuphandlers.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 from Products.CMFQuickInstallerTool import interfaces as qi_interfaces
 from Products.CMFPlone import interfaces as plone_interfaces
 from zope.interface import implements
