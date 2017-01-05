9c7047939c84234e1b28b97290edc64939830baf
Héctor Velarde <hvelarde@yahoo.com>
2017-01-05 17:22:07 -0200

    Add missing upgrade step (#93)

--- sc/social/like/upgrades/__init__.py
+++ sc/social/like/upgrades/__init__.py
@@ -3,14 +3,14 @@ from plone import api
 from sc.social.like.logger import logger
 
 
-def cook_css_resources(context):
+def cook_css_resources(context):  # pragma: no cover
     """Cook css resources."""
     css_tool = api.portal.get_tool('portal_css')
     css_tool.cookResources()
     logger.info('CSS resources were cooked')
 
 
-def cook_javascript_resources(context):
+def cook_javascript_resources(context):  # pragma: no cover
     """Cook javascript resources."""
     js_tool = api.portal.get_tool('portal_javascripts')
     js_tool.cookResources()
