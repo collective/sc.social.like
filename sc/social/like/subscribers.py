41f1653409503fbb859b6e0ad04045051a871946
hvelarde <hector.velarde@gmail.com>
2017-03-13 11:05:19 -0300

    Refactor the way we deal with recursion (HT @rodfersou)

--- sc/social/like/subscribers.py
+++ sc/social/like/subscribers.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Package event subscribers.
+"""Event subscribers.
 
 Plone 5 includes a new social media configlet that duplicates some
 records already included in this package. To deal with that we use
@@ -15,27 +15,33 @@ from sc.social.like.logger import logger
 from zope.component import getUtility
 from zope.schema.interfaces import WrongType
 
+import traceback
+
 
 # redundant fields so far
 FIELDS = ('facebook_app_id', 'facebook_username', 'twitter_username')
 
-# used to avoid RuntimeError due to infinite recursion
-_FLAG = False
+# used to deal with recursion
+FN_NAME = 'social_media_record_synchronizer'
 
 
 def social_media_record_synchronizer(event):
-    """Synchronize Plone 5 social media records if needed."""
+    """Synchronize Plone 5 social media records if needed.
+
+    An aditional IRecordModifiedEvent is fired by this function; we
+    deal with that to avoid RuntimeError due to infinite recursion.
+    """
+    # how many times the name of this function appears on the stack?
+    stack = [l[2] for l in traceback.extract_stack() if l[2] == FN_NAME]
+    if len(stack) > 1:
+        return  # we've been here before
+
     # subscribers are registered even if packages are not installed
     qi = api.portal.get_tool('portal_quickinstaller')
     if not qi.isProductInstalled(PROJECTNAME):
         return
 
     if not IS_PLONE_5:
-        return
-
-    # check if we've been here before as part of same event processing
-    global _FLAG
-    if _FLAG:
         return
 
     logger.debug(u'Processing: ' + repr(event.record))
@@ -58,15 +64,12 @@ def social_media_record_synchronizer(event):
         return
 
     registry = getUtility(IRegistry)
-    # we're going to modify a record and this will fire the same event
-    # we need to avoid RuntimeError due to infinite recursion
-    _FLAG = True
+    # this will fire the aditional IRecordModifiedEvent
     try:
         registry[record] = str(event.record.value)
     except WrongType:
         # Plone 5 declares records as TextLine
         registry[record] = unicode(event.record.value)
-    _FLAG = False
 
     logger.debug('{0} was synchronized; new value is "{1}"'.format(
         repr(registry.records[record]), event.record.value))
