26321fe86bbb4cdc6eb45a2cf2876c2da52edbee
hvelarde <hector.velarde@gmail.com>
2017-09-05 09:36:28 -0300

    Clean up WhatsApp plugin code

--- sc/social/like/plugins/whatsapp/browser.py
+++ sc/social/like/plugins/whatsapp/browser.py
@@ -1,35 +1,24 @@
 # -*- coding:utf-8 -*-
+"""Helper view to generate WhatsApp widget."""
+from Acquisition import aq_inner
 from plone import api
-from plone.api.exc import InvalidParameterError
 from Products.CMFPlone.utils import safe_unicode
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.PythonScripts.standard import url_quote
 from sc.social.like.interfaces import ISocialLikeSettings
 from sc.social.like.utils import get_language
-from zope.component import getMultiAdapter
 
 
 class PluginView(BrowserView):
-
-    typebutton = ''
-    language = 'en'
+    """Helper view to generate WhatsApp widget."""
 
     metadata = None
     plugin = link = ViewPageTemplateFile('templates/plugin.pt')
 
     def __init__(self, context, request):
-        self.context = context
+        self.context = aq_inner(context)
         self.request = request
-        # FIXME: the following could rise unexpected exceptions
-        #        move it to a new setup() method
-        #        see: http://docs.plone.org/develop/plone/views/browserviews.html#creating-a-view
-        self.portal_state = getMultiAdapter(
-            (self.context, self.request), name=u'plone_portal_state')
-        self.portal = self.portal_state.portal()
-        self.site_url = self.portal_state.portal_url()
-        self.portal_title = self.portal_state.portal_title()
-        self.url = context.absolute_url()
         self.language = get_language(context)
         data = url_quote(u'{0} - {1}'.format(
             safe_unicode(self.context.title), self.context.absolute_url()))
@@ -39,10 +28,7 @@ class PluginView(BrowserView):
     def klass(self):
         klass = 'whatsapp'
         record = ISocialLikeSettings.__identifier__ + '.do_not_track'
-        try:
-            do_not_track = api.portal.get_registry_record(record)
-        except InvalidParameterError:
-            do_not_track = False
+        do_not_track = api.portal.get_registry_record(record, default=False)
         if do_not_track:
             klass += ' link'
         return klass
