dc2a7991dadb80614ba1ff11b0327f82c9012641
hvelarde <hector.velarde@gmail.com>
2017-09-05 09:35:26 -0300

    Clean up Email plugin code

--- sc/social/like/plugins/email/browser.py
+++ sc/social/like/plugins/email/browser.py
@@ -1,15 +1,13 @@
 # -*- coding:utf-8 -*-
+"""Helper view to generate Email widget."""
 from plone import api
-from plone.api.exc import InvalidParameterError
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from sc.social.like.interfaces import ISocialLikeSettings
 
 
 class PluginView(BrowserView):
-
-    typebutton = ''
-    language = 'en'
+    """Helper view to generate Email widget."""
 
     metadata = None
     plugin = link = ViewPageTemplateFile('plugin.pt')
@@ -18,10 +16,7 @@ class PluginView(BrowserView):
     def klass(self):
         klass = 'share-by-email pat-plone-modal'
         record = ISocialLikeSettings.__identifier__ + '.do_not_track'
-        try:
-            do_not_track = api.portal.get_registry_record(record)
-        except InvalidParameterError:
-            do_not_track = False
+        do_not_track = api.portal.get_registry_record(record, default=False)
         if do_not_track:
             klass += ' link'
         return klass
