65af89ce8ae54f6efd4902464437a7f6abb5b465
p_991010 <9Jmr$M30x>
2017-09-15 16:35:45 -0300

    #130 - Changed as suggested #discussion_r139045269

--- sc/social/like/browser/imagefallback.py
+++ sc/social/like/browser/imagefallback.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from plone.formwidget.namedfile.converter import b64decode_file
 from plone.namedfile.browser import Download
 from plone.namedfile.file import NamedImage
-from plone.registry.interfaces import IRegistry
 from sc.social.like.interfaces import ISocialLikeSettings
-from zope.component import getUtility
 
 
 class ImageFallBack(Download):
 
-    def __init__(self, context, request):
-        super(ImageFallBack, self).__init__(context, request)
-        self.filename = None
-        self.data = None
+    filename = None
+    data = None
 
-        registry = getUtility(IRegistry)
-        settings = registry.forInterface(ISocialLikeSettings)
-        if getattr(settings, 'image_fallback', False):
-            filename, data = b64decode_file(settings.image_fallback)
+    def __call__(self):
+        self.setup()
+        super(ImageFallBack, self).__call__()
+
+    def setup(self):
+        fallback_image = api.portal.get_registry_record('fallback_image', interface=ISocialLikeSettings)
+        if fallback_image:
+            filename, data = b64decode_file(fallback_image)
             data = NamedImage(data=data, filename=filename)
             self.data = data
             self.filename = filename
