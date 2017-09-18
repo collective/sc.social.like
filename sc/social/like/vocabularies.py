e79a1c9ad9ff96f593e162f22dc5f5b85118fe91
claytonc.sousa@gmail.com <GabyDuda2009>
2017-09-18 11:02:16 -0300

    #130 - rebase

--- sc/social/like/vocabularies.py
+++ sc/social/like/vocabularies.py
@@ -2,6 +2,8 @@
 from plone.app.imaging.utils import getAllowedSizes
 from plone.memoize import forever
 from sc.social.like import LikeMessageFactory as _
+from sc.social.like.config import OG_LEAD_IMAGE_MIN_HEIGHT
+from sc.social.like.config import OG_LEAD_IMAGE_MIN_WIDTH
 from sc.social.like.plugins import IPlugin
 from zope.component import getUtilitiesFor
 from zope.interface import implementer
@@ -9,12 +11,6 @@ from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
-
-
-# ADD IN CONFIG.PY
-LEAD_IMAGE_MIN_ASPECT_RATIO = 1.33
-LEAD_IMAGE_MIN_HEIGHT = 315
-LEAD_IMAGE_MIN_WIDTH = 600
 
 
 @implementer(IVocabularyFactory)
@@ -57,11 +53,11 @@ FacebookButtonsVocabulary = SimpleVocabulary([
 
 
 @provider(IVocabularyFactory)
-def ImageScalesVocabulary(object):
+def image_scales_vocabulary(object):
     """Obtains available scales from plone.app.imaging"""
     terms = []
     for scale, (width, height) in getAllowedSizes().iteritems():
-        if width >= LEAD_IMAGE_MIN_WIDTH and height >= LEAD_IMAGE_MIN_HEIGHT:
+        if width >= OG_LEAD_IMAGE_MIN_WIDTH and height >= OG_LEAD_IMAGE_MIN_HEIGHT:
             translated = _(
                 'imagescale_{0:s}'.format(scale),
                 default='{0:s} ${{width}}x${{height}}'.format(scale),
