7413a104339fe48d58dd5c8bb13e51561ab2e6d0
Mikel Larreategi <mlarreategi@codesyntax.com>
2017-11-29 11:36:39 +0100

    fix upgrade step when there are no plone.app.tiles records on registry

--- sc/social/like/upgrades/v3041/__init__.py
+++ sc/social/like/upgrades/v3041/__init__.py
@@ -6,7 +6,8 @@ from sc.social.like.logger import logger
 
 def register_cover_tiles(setup_tool):
     """Register collective.cover tiles."""
-    registered = api.portal.get_registry_record('plone.app.tiles')
-    [registered.append(t) for t in TILES if t not in registered]
-    assert set(registered) & set(TILES) == set(TILES)
-    logger.info('Tiles for collective.cover were registered')
+    registered = api.portal.get_registry_record('plone.app.tiles', default=None)
+    if registered is not None:
+        [registered.append(t) for t in TILES if t not in registered]
+        assert set(registered) & set(TILES) == set(TILES)
+        logger.info('Tiles for collective.cover were registered')
