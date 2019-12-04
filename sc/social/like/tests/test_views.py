ef531ad6bcd7b446032524a3b98eaf08ada5bf83
idgserpro <idg@serpro.gov.br>
2019-12-04 11:52:37 -0200

    Remove unused view social_likes_view
    Fix #176

--- sc/social/like/tests/test_views.py
+++ sc/social/like/tests/test_views.py
@@ -1,36 +1,8 @@
 # -*- coding: utf-8 -*-
 from plone import api
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
-from sc.social.like.browser.socialikes import SocialLikes
 from sc.social.like.testing import INTEGRATION_TESTING
 
 import unittest
-
-
-class BrowserViewTestCase(unittest.TestCase):
-
-    layer = INTEGRATION_TESTING
-
-    def setUp(self):
-        self.portal = self.layer['portal']
-        self.request = self.layer['request']
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        self.portal.invokeFactory('Document', 'my-document')
-        self.document = self.portal['my-document']
-
-    def view(self, context=None):
-        context = context or self.portal
-        view = SocialLikes(context, self.request)
-        return view
-
-    def test_disabled_on_portal(self):
-        view = self.view(self.portal)
-        self.assertFalse(view.enabled)
-
-    def test_enabled_on_document(self):
-        view = self.view(self.document)
-        self.assertTrue(view.enabled)
 
 
 class FallBackImageViewTestCase(unittest.TestCase):
